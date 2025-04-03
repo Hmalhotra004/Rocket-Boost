ba795fd2fc69480dbe1dee12ab30160e7aa31d48
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-04-03 21:59:51 +0530

    scnene change

--- Assets/scripts/CollisionHandler.cs
+++ Assets/scripts/CollisionHandler.cs
@@ -5,16 +5,23 @@ public class CollisionHandler : MonoBehaviour {
     private void OnCollisionEnter(Collision other) {
         switch(other.gameObject.tag){
             case "Friendly":
-                Debug.Log("Friendly collision detected.");
                 break;
             case "Finish":
-                Debug.Log("W");
-                // Damage the enemy
+                Debug.Log("Level Complete!");
                 break;
             default:
             reloadLevel();
                 break;
         }
+    }
+
+    private void loadNextLevel(){
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        int nextScene = currentScene + 1;
+        if(nextScene == SceneManager.sceneCountInBuildSettings){
+            nextScene = 0;
+        }
+        SceneManager.LoadScene(nextScene);
     }
 
     private void reloadLevel() {
