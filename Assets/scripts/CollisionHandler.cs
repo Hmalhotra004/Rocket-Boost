fab74a2c25b7d10469f5a8572afc040fb7d89068
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-04-04 13:15:17 +0530

    bug fix

--- Assets/scripts/CollisionHandler.cs
+++ Assets/scripts/CollisionHandler.cs
@@ -7,12 +7,16 @@ public class CollisionHandler : MonoBehaviour {
     [SerializeField] AudioClip successSound;
     AudioSource audioSource;
 
+    bool isContollable = true;
+
     private void Start() {
         audioSource = GetComponent<AudioSource>();
     }
 
     private void OnCollisionEnter(Collision other) {
-        switch(other.gameObject.tag){
+        if(!isContollable) return;
+
+        switch(other.gameObject.tag) {
             case "Friendly":
                 break;
             case "Finish":
@@ -25,21 +29,25 @@ public class CollisionHandler : MonoBehaviour {
     }
 
     private void startSuccessSequence() {
+        isContollable = false;
+        audioSource.Stop();
         GetComponent<Movement>().enabled = false;
         audioSource.PlayOneShot(successSound);
         Invoke("loadNextLevel",levelLoadDelay);
     }
 
     private void startCrashSequence() {
+        isContollable = false;
+        audioSource.Stop();
         GetComponent<Movement>().enabled = false;
         audioSource.PlayOneShot(crashSound);
         Invoke("reloadLevel", levelLoadDelay);
     }
 
-    private void loadNextLevel(){
+    private void loadNextLevel() {
         int currentScene = SceneManager.GetActiveScene().buildIndex;
         int nextScene = currentScene + 1;
-        if(nextScene == SceneManager.sceneCountInBuildSettings){
+        if(nextScene == SceneManager.sceneCountInBuildSettings) {
             nextScene = 0;
         }
         SceneManager.LoadScene(nextScene);
